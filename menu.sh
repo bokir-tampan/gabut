5c7f453ca10c9d32d96f1583c4563a0fe23c5846
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2022-06-20 09:42:23 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[40;1;37m|              • XRAY  MENU •               |\E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[0;34m addws                >   Add User Vmess \033[0m"
-echo -e "\033[0;34m addvless             >   Add User Vless \033[0m"
-echo -e "\033[0;34m addss                >   Add User Sodosok ws/grpc  \033[0m"
-echo -e "\033[0;34m addtr                >   Add User Vmess \033[0m"
-echo -e "\033[0;34m cek-user             >   Cek All User  \033[0m"
-echo -e "\033[0;34m renew                >   Renew All User \033[0m"
-echo -e "\033[0;34m del-user             >   Delete All  User  \033[0m"
-echo -e "\033[0;34m cek-usage            >   Cek Usage User Xray  \033[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[0;34m              Lope Janda Bahenol<3 <3 \033[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;41;36m            XRAY - MENU            \E[0m"
+echo -e "\e[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""   
+echo -e " [\e[36m•1\e[0m] Add Vmess WS/GRPC  "
+echo -e " [\e[36m•2\e[0m] Add Vless WS/GRPC  "
+echo -e " [\e[36m•3\e[0m] Add Trojan WS/GRPC  "
+echo -e " [\e[36m•4\e[0m] Add Sodosok WS/GRPC  "
+echo -e " [\e[36m•5\e[0m] Del User"
+echo -e " [\e[36m•6\e[0m] Cek User"
+echo -e " [\e[36m•7\e[0m] Renew User"
+echo -e " [\e[36m•8\e[0m] Cek Usage"
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e   ""
+echo -e  "Press x or [ Ctrl+C ] • To-Exit"
+echo -e ""
+echo -e "\e[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; addws;;
+2) clear ; addvless ;;
+3) clear ; addtr;;
+4) clear ; addss ;;
+5) clear ; del-user ;;
+6) clear ; cek-user ;;
+7) clear ; renew-user ;;
+8) clear ; cek-usage ;;
+0) clear ; menu ;;
+x) exit ;;
+*) echo -e "" ; echo "Press any key to back on menu" ; sleep 1 ; menu ;;
+esac
